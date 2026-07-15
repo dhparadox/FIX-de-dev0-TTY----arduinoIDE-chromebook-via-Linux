027942f9d04e337c5224a59442b85b32985ecb27
Diegho Pagliarim Crestani <diegho.crestani@outlook.com>
2026-07-15 18:15:40 -0300

    Update setup-Rpico---yes-to-all.sh

--- docs/instaladores/setup-Rpico---yes-to-all.sh
+++ docs/instaladores/setup-Rpico---yes-to-all.sh
@@ -3,7 +3,8 @@
 # # # Script para agilizar o ato de configurar o ambiente de SDK do Raspberry Pi Pico. # # #
 
 set -e # Grarante que caso haja falhas na hora de instalar, pare de executar
-sudo apt install -y \
+
+sudo apt install {variavel_flag-y}\
 build-essential \
 cmake \
 gcc-arm-none-eabi \
@@ -20,3 +21,23 @@ arm-none-eabi-gcc --version
 
 echo "========== CONFERINDO CMAKE =========="
 cmake --version
+
+[aqui SE o usuario nao colocou a flag -y, deve aparecer uma mensagem perguntando o seguinte "verifique se houve erro se instalacao, caso esteje tudo ok digite o caractere 'Y'"]
+echo "========== CRIANDO DIRETORIO DE DESENVOLVIMENTO =========="
+mkdir -p ~/dev/pico
+cd ~/dev/pico
+echo "========== CLONANDO O SDK (Software Development Kit)"
+git clone https://github.com/raspberrypi/pico-sdk.git
+clear
+echo "========== INDO PARA O DIRETORIO DO SDK ==========\n"
+cd pico-sdk
+echo "========== BAIXANDO SUBMODULOS =========="
+git submodule update --init
+clear
+echo "========== CONFIGURANDO A VARIAVEL DE AMBIENTE DO SDK =========="
+echo 'export PICO_SDK_PATH=$HOME/dev/pico/pico-sdk' >> ~/.bashrc
+source ~/.bashrc
+echo $PICO_SDK_PATH
+clear
+echo "========== FIM DO SCRIPT ==========\n\ndhdxs\n"
+
